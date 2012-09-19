5ef8040cad060b65d00849b96194ec2e4e8f10fa
Jimmy Theis <jimmy@jetheis.com>
2012-09-18 21:52:45 -0400

    Still reoganizing

--- passfactory/Core.js
+++ passfactory/Core.js
@@ -1,3 +1,10 @@
-define('Core', [], function() {
+require.config({
+    paths: {
+        'underscore': 'external/underscore',
+        'sha1': 'external/sha1'
+    }
+});
 
+define('Core', [], function() {
+    
 });
