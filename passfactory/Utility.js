4f85d2d33e3d2278f2f7c261c1b61efc0f1c2b14
Jimmy Theis <jimmy@jetheis.com>
2012-09-21 01:33:50 -0400

    Add base64 encoding

--- passfactory/Utility.js
+++ passfactory/Utility.js
@@ -17,6 +17,64 @@ define(['underscore', 'sha1', 'zip'], function(_, CryptoJS, JSZip) {
             fileReader.readAsBinaryString(file);
         },
 
+        /**
+         * From: http://www.webtoolkit.info/javascript-base64.html
+         */
+        base64: function(str) {
+            _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+            var utf8_encode = function(s) {
+                s = s.replace(/\r\n/g,"\n");
+                var utftext = "";
+                for (var n = 0; n < s.length; n++) {
+                    var c = s.charCodeAt(n);
+                    if (c < 128) utftext += String.fromCharCode(c);
+                    else if (c > 127 && c < 2048) {
+                        utftext += String.fromCharCode((c >> 6) | 192);
+                        utftext += String.fromCharCode((c & 63) | 128);
+                    }
+                    else {
+                        utftext += String.fromCharCode((c >> 12) | 224);
+                        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                        utftext += String.fromCharCode((c & 63) | 128);
+                    }
+                }
+                return utftext;
+            };
+ 
+            var output = "";
+            var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+            var i = 0;
+ 
+            str = utf8_encode(str);
+ 
+            while (i < str.length) {
+ 
+                chr1 = str.charCodeAt(i++);
+                chr2 = str.charCodeAt(i++);
+                chr3 = str.charCodeAt(i++);
+ 
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+ 
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+ 
+                output = output +
+                _keyStr.charAt(enc1) + _keyStr.charAt(enc2) +
+                _keyStr.charAt(enc3) + _keyStr.charAt(enc4);
+ 
+            }
+ 
+            return output;
+        },
+
+
         isValidFieldValue: function(val) {
             return this.isCorrectType(val, String) ||
                    this.isCorrectType(val, Number) ||
