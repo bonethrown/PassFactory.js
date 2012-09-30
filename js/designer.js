076b14434c190bb0c50900d1af93de04313d00f4
Jimmy Theis <jimmy@jetheis.com>
2012-09-30 11:03:02 -0400

    Not sure what I changed. Too lazy.

--- js/designer.js
+++ js/designer.js
@@ -13,6 +13,68 @@ function Designer() {
 
     this._generateSerialNumberButton = $('#generateSerialNumber');
 
+    this._logoTextInput = $('#logoText');
+
+    this._backgroundImageButton = $('#backgroundImageButton');
+    this._backgroundImageClearButton = $('#backgroundImageClearButton');
+    this._backgroundImageInputContainer = $('#backgroundImageInputContainer');
+    this._backgroundImageInput = $('#backgroundImageInput');
+
+    this._retinaBackgroundImageButton = $('#retinaBackgroundImageButton');
+    this._retinaBackgroundImageClearButton = $('#retinaBackgroundImageClearButton');
+    this._retinaBackgroundImageInputContainer = $('#retinaBackgroundImageInputContainer');
+    this._retinaBackgroundImageInput = $('#retinaBackgroundImageInput');
+
+    this._footerImageButton = $('#footerImageButton');
+    this._footerImageClearButton = $('#footerImageClearButton');
+    this._footerImageInputContainer = $('#footerImageInputContainer');
+    this._footerImageInput = $('#footerImageInput');
+
+    this._retinaFooterImageButton = $('#retinaFooterImageButton');
+    this._retinaFooterImageClearButton = $('#retinaFooterImageClearButton');
+    this._retinaFooterImageInputContainer = $('#retinaFooterImageInputContainer');
+    this._retinaFooterImageInput = $('#retinaFooterImageInput');
+
+    this._iconImageButton = $('#iconImageButton');
+    this._iconImageClearButton = $('#iconImageClearButton');
+    this._iconImageInputContainer = $('#iconImageInputContainer');
+    this._iconImageInput = $('#iconImageInput');
+
+    this._retinaIconImageButton = $('#retinaIconImageButton');
+    this._retinaIconImageClearButton = $('#retinaIconImageClearButton');
+    this._retinaIconImageInputContainer = $('#retinaIconImageInputContainer');
+    this._retinaIconImageInput = $('#retinaIconImageInput');
+
+    this._logoImageButton = $('#logoImageButton');
+    this._logoImageClearButton = $('#logoImageClearButton');
+    this._logoImageInputContainer = $('#logoImageInputContainer');
+    this._logoImageInput = $('#logoImageInput');
+
+    this._retinaLogoImageButton = $('#retinaLogoImageButton');
+    this._retinaLogoImageClearButton = $('#retinaLogoImageClearButton');
+    this._retinaLogoImageInputContainer = $('#retinaLogoImageInputContainer');
+    this._retinaLogoImageInput = $('#retinaLogoImageInput');
+
+    this._stripImageButton = $('#stripImageButton');
+    this._stripImageClearButton = $('#stripImageClearButton');
+    this._stripImageInputContainer = $('#stripImageInputContainer');
+    this._stripImageInput = $('#stripImageInput');
+
+    this._retinaStripImageButton = $('#retinaStripImageButton');
+    this._retinaStripImageClearButton = $('#retinaStripImageClearButton');
+    this._retinaStripImageInputContainer = $('#retinaStripImageInputContainer');
+    this._retinaStripImageInput = $('#retinaStripImageInput');
+
+    this._thumbnailImageButton = $('#thumbnailImageButton');
+    this._thumbnailImageClearButton = $('#thumbnailImageClearButton');
+    this._thumbnailImageInputContainer = $('#thumbnailImageInputContainer');
+    this._thumbnailImageInput = $('#thumbnailImageInput');
+
+    this._retinaThumbnailImageButton = $('#retinaThumbnailImageButton');
+    this._retinaThumbnailImageClearButton = $('#retinaThumbnailImageClearButton');
+    this._retinaThumbnailImageInputContainer = $('#retinaThumbnailImageInputContainer');
+    this._retinaThumbnailImageInput = $('#retinaThumbnailImageInput');
+
     this._generatePassButton = $('#generateButton');
     this._validationErrorContainer = $('#validationErrorContainer');
     this._downloadButtonContainer = $('#downloadLinkContainer');
@@ -22,16 +84,15 @@ function Designer() {
 
 Designer.prototype = {
     initialize: function() {
-        var requiredStringValidator = function(e) {
-        };
-
         // Register input change handlers
-        this._descriptionInput.change(this._stringInputValidator());
-        this._organizationNameInput.change(this._stringInputValidator());
-        this._passTypeIdentifierInput.change(this._stringInputValidator());
-        this._teamIdentifierInput.change(this._stringInputValidator());
-        this._serialNumberInput.change(this._stringInputValidator());
-        this._serialNumberInput.change(this._stringInputValidator());
+        this._descriptionInput.change(this._stringInputValidator(true));
+        this._organizationNameInput.change(this._stringInputValidator(true));
+        this._passTypeIdentifierInput.change(this._stringInputValidator(true));
+        this._teamIdentifierInput.change(this._stringInputValidator(true));
+        this._serialNumberInput.change(this._stringInputValidator(true));
+        this._serialNumberInput.change(this._stringInputValidator(true));
+
+        this._logoTextInput.change(this._stringInputValidator(false));
 
         // Register click handlers
         this._generateSerialNumberButton.click(this.generateSerialNumber.bind(this));
@@ -39,12 +100,85 @@ Designer.prototype = {
         this._hookUpFileButtonHandlers(this._certificateFileButton,
                                        this._certificateFileClearButton,
                                        this._certificateFileInput,
-                                       this._certificateFileInputContainer);
+                                       this._certificateFileInputContainer,
+                                       true);
+
+        this._hookUpFileButtonHandlers(this._backgroundImageButton,
+                                       this._backgroundImageClearButton,
+                                       this._backgroundImageInput,
+                                       this._backgroundImageInputContainer,
+                                       false);
+
+        this._hookUpFileButtonHandlers(this._retinaBackgroundImageButton,
+                                       this._retinaBackgroundImageClearButton,
+                                       this._retinaBackgroundImageInput,
+                                       this._retinaBackgroundImageInputContainer,
+                                       false);
+
+        this._hookUpFileButtonHandlers(this._footerImageButton,
+                                       this._footerImageClearButton,
+                                       this._footerImageInput,
+                                       this._footerImageInputContainer,
+                                       false);
+
+        this._hookUpFileButtonHandlers(this._retinaFooterImageButton,
+                                       this._retinaFooterImageClearButton,
+                                       this._retinaFooterImageInput,
+                                       this._retinaFooterImageInputContainer,
+                                       false);
+
+        this._hookUpFileButtonHandlers(this._iconImageButton,
+                                       this._iconImageClearButton,
+                                       this._iconImageInput,
+                                       this._iconImageInputContainer,
+                                       false);
+
+        this._hookUpFileButtonHandlers(this._retinaIconImageButton,
+                                       this._retinaIconImageClearButton,
+                                       this._retinaIconImageInput,
+                                       this._retinaIconImageInputContainer,
+                                       false);
+
+        this._hookUpFileButtonHandlers(this._logoImageButton,
+                                       this._logoImageClearButton,
+                                       this._logoImageInput,
+                                       this._logoImageInputContainer,
+                                       false);
+
+        this._hookUpFileButtonHandlers(this._retinaLogoImageButton,
+                                       this._retinaLogoImageClearButton,
+                                       this._retinaLogoImageInput,
+                                       this._retinaLogoImageInputContainer,
+                                       false);
+
+        this._hookUpFileButtonHandlers(this._stripImageButton,
+                                       this._stripImageClearButton,
+                                       this._stripImageInput,
+                                       this._stripImageInputContainer,
+                                       false);
+
+        this._hookUpFileButtonHandlers(this._retinaStripImageButton,
+                                       this._retinaStripImageClearButton,
+                                       this._retinaStripImageInput,
+                                       this._retinaStripImageInputContainer,
+                                       false);
+
+        this._hookUpFileButtonHandlers(this._thumbnailImageButton,
+                                       this._thumbnailImageClearButton,
+                                       this._thumbnailImageInput,
+                                       this._thumbnailImageInputContainer,
+                                       false);
+
+        this._hookUpFileButtonHandlers(this._retinaThumbnailImageButton,
+                                       this._retinaThumbnailImageClearButton,
+                                       this._retinaThumbnailImageInput,
+                                       this._retinaThumbnailImageInputContainer,
+                                       false);
 
         this._generatePassButton.click(this.generatePass.bind(this));
     },
 
-    _stringInputValidator: function(predicate) {
+    _stringInputValidator: function(required, predicate) {
         return function(e) {
             var input = $(e.target);
             var container = $(input.parent().parent());
@@ -55,12 +189,12 @@ Designer.prototype = {
                 container.addClass('success');
             } else {
                 container.removeClass('success');
-                container.addClass('error');
+                if (required) container.addClass('error');
             }
         };
     },
 
-    _hookUpFileButtonHandlers: function(fileButton, clearButton, input, container) {
+    _hookUpFileButtonHandlers: function(fileButton, clearButton, input, container, required) {
         // The input change handler needs to be reusable
         var inputChange = function(e) {
             var input = e.target;
@@ -78,12 +212,12 @@ Designer.prototype = {
                 clearButton.removeClass('hidden');
             } else {
 
-                fileButton.html('Choose File <i class="icon-chevron-right icon-white"></i>');
+                fileButton.html('Choose File <i class="icon-chevron-right' + (required ? ' icon-white' : '') + '"></i>');
                 fileButton.removeClass('btn-success');
-                fileButton.addClass('btn-danger');
+                if (required) fileButton.addClass('btn-danger');
 
                 container.removeClass('success');
-                container.addClass('error');
+                if (required) container.addClass('error');
 
                 clearButton.addClass('hidden');
             }
@@ -197,6 +331,21 @@ Designer.prototype = {
                 teamIdentifier: this._teamIdentifierInput.val(),
                 serialNumber: this._serialNumberInput.val()
             });
+
+            if (this._logoTextInput.val()) pass.logoText = this._logoTextInput.val();
+            
+            if (this._backgroundImageInput.get(0).files.length > 0) pass.backgroundImage = this._backgroundImageInput.get(0).files[0];
+            if (this._retinaBackgroundImageInput.get(0).files.length > 0) pass.retinaBackgroundImage = this._retinaBackgroundImageInput.get(0).files[0];
+            if (this._footerImageInput.get(0).files.length > 0) pass.footerImage = this._footerImageInput.get(0).files[0];
+            if (this._retinaFooterImageInput.get(0).files.length > 0) pass.retinaFooterImage = this._retinaFooterImageInput.get(0).files[0];
+            if (this._iconImageInput.get(0).files.length > 0) pass.iconImage = this._iconImageInput.get(0).files[0];
+            if (this._retinaIconImageInput.get(0).files.length > 0) pass.retinaIconImage = this._retinaIconImageInput.get(0).files[0];
+            if (this._logoImageInput.get(0).files.length > 0) pass.logoImage = this._logoImageInput.get(0).files[0];
+            if (this._retinaLogoImageInput.get(0).files.length > 0) pass.retinaLogoImage = this._retinaLogoImageInput.get(0).files[0];
+            if (this._stripImageInput.get(0).files.length > 0) pass.stripImage = this._stripImageInput.get(0).files[0];
+            if (this._retinaStripImageInput.get(0).files.length > 0) pass.retinaStripImage = this._retinaStripImageInput.get(0).files[0];
+            if (this._thumbnailImageInput.get(0).files.length > 0) pass.thumbnailImage = this._thumbnailImageInput.get(0).files[0];
+            if (this._retinaThumbnailImageInput.get(0).files.length > 0) pass.retinaThumbnailImage = this._retinaThumbnailImageInput.get(0).files[0];
             
             pass.toAppleScript(function(script) {
                 this._downloadButtonContainer.downloadify({
