5ca3398442d445a1a1b9af46da2c042459a171d8
Jimmy Theis <jimmy@jetheis.com>
2012-10-07 10:13:49 -0400

    Add date and number style models

---
+++ passfactory/model/DateStyle.js
@@ -0,0 +1,14 @@
+define(function() {
+
+	"use strict";
+
+	var DateStyle = {
+		None: 'PKDateStyleNone',
+		Short: 'PKDateStyleShort',
+		Medium: 'PKDateStyleMedium',
+		Long: 'PKDateStyleLong',
+		Full: 'PKDateStyleFull'
+	};
+
+	return Object.freeze(DateStyle);
+});
---
+++ passfactory/model/NumberStyle.js
@@ -0,0 +1,13 @@
+define(function() {
+
+	"use strict";
+
+	var NumberStyle = {
+		Decimal: 'PKNumberStyleDecimal',
+		Percent: 'PKNumberStylePercent',
+		Scientific: 'PKNumberStyleScientific',
+		SpellOut: 'PKNumberStyleSpellOut'
+	};
+
+	return Object.freeze(NumberStyle);
+});
