6a550ebedee72e03070243d43311cb968f78c9f7
Jimmy Theis <jimmy@jetheis.com>
2012-10-10 22:49:40 -0400

    Add toJSON override to BoardingPass

--- passfactory/model/BoardingPass.js
+++ passfactory/model/BoardingPass.js
@@ -8,7 +8,10 @@ function(Utility, Pass, PassStyle, TransitType) {
     "use strict";
 
     function BoardingPass(args) {
-        Pass.call(this, args);
+
+        this._super = Pass;
+
+        this._super.call(this, args);
         
         if (args) {
             if (args.transitType) this.transitType = args.transitType;
@@ -16,7 +19,16 @@ function(Utility, Pass, PassStyle, TransitType) {
     }
 
     BoardingPass.prototype = Object.create(new Pass(), {
-    
+        toJSON: { value: function() {
+
+            if (!this.transitType) throw new Error('Pass is not ready to be serialized. Transit type is not defined.');
+
+            var result = this._super.prototype.toJSON.apply(this, arguments);
+
+            result.boardingPass.transitType = this.transitType;
+
+            return result;
+        }}
     });
 
     Object.defineProperties(BoardingPass.prototype, {
