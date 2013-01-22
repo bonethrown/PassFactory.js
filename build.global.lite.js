e6bc9c6d2289f9aa7009fef3938cf12578afc1bd
Jimmy Theis <jimmy@jetheis.com>
2013-01-22 08:20:45 -0500

    Add 'lite' to build.global.lite.js

--- build.global.lite.js
+++ build.global.lite.js
@@ -3,7 +3,7 @@
     name: 'almond',
     include: ['_globallite'],
     insertRequire: ['_globallite'],
-    out: 'build/passfactory-' + fs.readFileSync('VERSION', 'utf8') + '.js',
+    out: 'build/passfactorylite-' + fs.readFileSync('VERSION', 'utf8') + '.js',
     wrap: {
         start: fs.readFileSync('BANNER', 'utf8').replace('VERSION', fs.readFileSync('VERSION', 'utf8')).replace('EDITION', 'Global export lite edition') + '(function() {',
         end: '}());'
